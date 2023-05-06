3150380d658588330d53691a07ee7de7a4227999
Asadbek <asadbekn443@gmail.com>
2023-05-06 03:28:12 -0400

    Initial Commit

---
+++ AutoslidingCarousel/AutoslidingCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoslidingCarouselApp.swift
+//  AutoslidingCarousel
+//
+//  Created by Asadbek Nematov on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AutoslidingCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
